0494862543588edbd2219e01c5660b065d041b56
fuuilstacker <mirohsnichenkopavlo802@gmail.com>
2024-09-01 18:09:24 +0300

    first commitdd

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const WebSocket = require('ws')
-const server = new WebSocket.Server({ port: 9090 })
+const server = new WebSocket.Server({ port: 9091 })
+const cors = require('cors')
 
 const clients = new Map()
+
+server.use(cors())
 
 server.on('connection', (socket) => {
 	const id = Date.now()
